7cbb1763807f81ad7d9e3ed36cc692900fefa75b
Jong Woo Nam <jwnam0709@gmail.com>
2020-02-04 12:24:55 -0800

    Update plot_sta.m
    adding graph stylings to make it look similar to the original lab's sta plots

--- src/Visualization/plot_sta.m
+++ src/Visualization/plot_sta.m
@@ -4,11 +4,26 @@ function plot_sta(sta_3d_mat, rf_temporal_len, time_step)
     figure;
     levels = -1:0.1:1;
     ts_max = time_step*rf_temporal_len;
+    sta_3d_mat = sta_3d_mat./max(max(max(abs(sta_3d_mat))));
+    [x_size, y_size, ~] = size(sta_3d_mat);
+    
     for i = 1:rf_temporal_len
-        subplot(5,ceil(rf_temporal_len/5),i);
-        contourf(rescale(sta_3d_mat(:,:,i), -1, 1), levels,'LineStyle', 'none');
+        subplot(1,rf_temporal_len,i);
+        data = zeros([x_size + 2, y_size + 2]);
+        data(2 : x_size + 1, 2 : y_size + 1) = sta_3d_mat(:,:,i);
+        contourf(data, levels,'LineStyle', 'none');
         colormap(color_map);
+        caxis([-1 1]);
         time_delay = ts_max-(i-1)*time_step;
         title(sprintf('t = %.2fms', time_delay*1000));
-    end            
+        set(gca, 'XTick', [], 'YTick', [], 'YDir', 'Rev', 'DataAspectRatioMode', 'manual', ...
+        'YLim', [1.5, y_size + 1.5], 'XLim', [1.5, x_size + 1.5], 'Color', 'k', 'DataAspectRatio', [1 1 1]);
+        for k = 1.5 : 1 : x_size + 1.5
+            line([k, k], [1.5, x_size + 1.5], 'Color', [0 0 0]);
+        end
+        for j = 1.5 : 1 : y_size + 1.5
+            line([1.5, y_size + 1.5], [j, j], 'Color', [0 0 0]);
+        end
+    end
+    
 end 
