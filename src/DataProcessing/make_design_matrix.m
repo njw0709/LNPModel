56c5114e9a843e0b56d71ab4f685afd528607951
Jong Woo Nam <jwnam0709@gmail.com>
2020-02-12 16:11:58 -0800

    fixing make_xdesign function

--- src/DataProcessing/make_design_matrix.m
+++ src/DataProcessing/make_design_matrix.m
@@ -1,13 +1,8 @@
 function xdsgn = make_design_matrix(sn_stim, rf_temporal_len)
     stim_len = size(sn_stim.stimulus,3);
-    flattened_stim = reshape(sn_stim.stimulus, [stim_len, sn_stim.config.gridsize^2]);
-    paddedStim = [zeros(rf_temporal_len-1,sn_stim.config.gridsize^2); flattened_stim]; % pad early bins of stimulus with zero
-    paddedStim = reshape(paddedStim,size(paddedStim,1)*size(paddedStim,2),1);
+    paddedStim = cat(3, zeros(sn_stim.config.gridsize,sn_stim.config.gridsize,rf_temporal_len-1), sn_stim.stimulus); % pad early bins of stimulus with zero
     xdsgn = zeros(stim_len,sn_stim.config.gridsize^2*rf_temporal_len);
     for j = 1:stim_len
-        %sliding window with jump size of gridsize^2 and window
-        %size of gridsize^2*rf_temporal_len
-        xdsgn(j,:) = paddedStim(1+(j-1)*sn_stim.config.gridsize^2:(j-1)* ...
-        sn_stim.config.gridsize^2+rf_temporal_len*sn_stim.config.gridsize^2)';
+        xdsgn(j,:) = reshape(paddedStim(:,:,j:j+9),[1,rf_temporal_len*sn_stim.config.gridsize^2]);
     end
 end
