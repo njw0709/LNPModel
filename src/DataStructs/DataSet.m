8c9f557dddf500605cdec0496cba95a652cdbba3
Jong Woo Nam <jwnam0709@gmail.com>
2020-02-14 12:34:19 -0800

    documentation

--- src/DataStructs/DataSet.m
+++ src/DataStructs/DataSet.m
@@ -1,23 +1,21 @@
 classdef DataSet
-    %DATASET Summary of this class goes here
-    %   Detailed explanation goes here
+    % Dataset data structure. It allows for easy joining of two datasets,
+    % as well as dividing the dataset into train and test sets.
     
     properties
-        variable_mat
-        label_vec
+        variable_mat % Design matrix with flattened stimulus concatenated as rows (dim: [num_stim, stim_length]) 
+        label_vec % binned spikes (dim: [num_stim, 1])
     end
     
     methods
         function obj = DataSet(variable_mat,label_vec)
             %DATASET Construct an instance of this class
-            %   Detailed explanation goes here
             obj.variable_mat = variable_mat;
             obj.label_vec = label_vec;
         end
         
         function [train_var, train_lab, test_var, test_lab] = divide_train_test_data(obj, train_pct)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
+            % Divides the dataset into train and test sets
             [data_len,~] = size(obj.variable_mat);
             idx = randperm(data_len);
             train_var = obj.variable_mat(idx(1:round(train_pct*data_len)),:); 
@@ -26,6 +24,7 @@ classdef DataSet
             test_lab = obj.label_vec(idx(round(train_pct*data_len)+1:end));
         end
         function obj = join(obj, dataset)
+            % joins two dataset objects
             obj.variable_mat = [obj.variable_mat;dataset.variable_mat];
             obj.label_vec = [obj.label_vec; dataset.label_vec];
         end
